6cee315610506fd6a2dfeb1197de5c0f9006052a
dlxrning@cn.ibm.com <nxinran@dlxrningcnibmcomdeMacBook-Pro.local>
2019-10-24 23:36:29 +0800

    siteDomain

--- app/config/setting.js
+++ app/config/setting.js
@@ -3,10 +3,10 @@
 let siteDomain = '127.0.0.1:5000/';
 switch (process.env.NODE_ENV) {
   case 'development':
-    siteDomain = 'https://www.guangtai.xyz/';
+    siteDomain = 'https://www.guangtai.xyz/sanchuang';
     break;
   case 'production':
-    siteDomain = 'https://www.guangtai.xyz/';
+    siteDomain = 'https://www.guangtai.xyz/sanchuang';
     break;
 }
 module.exports = {
