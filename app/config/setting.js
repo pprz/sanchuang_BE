ffdc799fde7be40b174177a9015b13e6cd8c3c09
dlxrning@cn.ibm.com <nxinran@dlxrningcnibmcomdeMacBook-Pro.local>
2019-10-24 23:13:46 +0800

    123

--- app/config/setting.js
+++ app/config/setting.js
@@ -12,7 +12,7 @@ switch (process.env.NODE_ENV) {
 module.exports = {
   port: 5000,
   siteDomain,
-  serverApi: 'https://www.guangtai.xyz/sanchuang',
+  serverApi: 'https://www.guangtai.com/sanchuang',
   countDefault: 10,
   pageDefault: 0,
   apiDir: 'app/api',
